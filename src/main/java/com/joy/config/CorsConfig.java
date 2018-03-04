84d87e5bb1b81a6e9a71154a4293dfafe9122d4a
myjoybar <myjoybar@gmail.com>
2018-03-04 22:26:15 +0800

    fix     Access-Control-Allow-Origin

--- src/main/java/com/joy/config/CorsConfig.java
+++ src/main/java/com/joy/config/CorsConfig.java
@@ -14,12 +14,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by joybar on 20/12/16.
  */
 @Configuration
-public class CorsConfig {
+public class CorsConfig extends WebMvcConfigurerAdapter {
     private CorsConfiguration buildConfig() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.addAllowedOrigin("*"); // 1
@@ -35,6 +37,18 @@ public class CorsConfig {
         return new CorsFilter(source);
     }
 
+    /**
+     *  Access-Control-Allow-Origin 的前端问题解决
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                .maxAge(3600);
+    }
 
 //    /**
 //     * it's for set http url auto change to https
